5973b9fae18db71f97e736d5c8af7aee2f9a5324
scaria-mathew <scariamathew2006@gmail.com>
2025-07-10 14:26:59 +0530

    Version 1

---
+++ rect_area.py
@@ -0,0 +1,9 @@
+length = int(input("Enter length: "))
+breadth = int(input("Enter breadth: "))
+
+perimeter = 2 * (length + breadth)
+
+area = length * breadth
+
+print("Perimeter: ", perimeter)
+print("Area: ", area)
